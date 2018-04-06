0c612f19782cef2c4c3eeed2a97f02d1d022f7da
Jan  Bolmer <janbolmer@Jans-MacBook-Pro.local>
2018-04-06 16:50:08 +0100

    test

--- f_cal.py
+++ f_cal.py
@@ -5,25 +5,24 @@ import argparse, sys
 
 
 def f_cal(nh2, nh2_err, nh, nh_err, logbool):
-	'''
-	calculate molecular hydrogen fraction and error
-	'''
+    '''
+    calculate molecular hydrogen fraction and error
+    '''
 
-	f1 = 2 * 10**nh2
-	f2 = 2 * 10**nh2 + 10**nh
-	f = f1/f2
+    f1 = 2 * 10**nh2
+    f2 = 2 * 10**nh2 + 10**nh
+    f = f1/f2
 
-	f1_err = 2 * 10**(nh2-nh2_err)
-	f2_err = 2 * 10**(nh2-nh2_err) + 10**(nh+nh_err)
-	f_err = f1_err/f2_err
+    f1_err = 2 * 10**(nh2-nh2_err)
+    f2_err = 2 * 10**(nh2-nh2_err) + 10**(nh+nh_err)
+    f_err = f1_err/f2_err
 
-	if logbool == True:
-		f, f_err = np.log10(f), abs(np.log10(f_err))
-	else:
+    if logbool == True:
+        f, f_err = np.log10(f), abs(np.log10(f_err))
+    else:
+        pass
 
-		pass
-
-	return f, f_err
+    return f, f_err
 
 
 if __name__ == "__main__":
@@ -31,10 +30,10 @@ if __name__ == "__main__":
     #writecmd("vinspec_old.dat")
 
     '''
-	Parsing commend line arguments:
-	-nh2: Molecular Hydrogen column denisty with error
-	-nh: Hydrogen column denisty with error
-	-log: return logarythmic value, default: True
+    Parsing commend line arguments:
+    -nh2: Molecular Hydrogen column denisty with error
+    -nh: Hydrogen column denisty with error
+    -log: return logarythmic value, default: True
     '''
 
     parser = argparse.ArgumentParser()
